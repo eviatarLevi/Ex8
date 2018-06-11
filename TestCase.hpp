9ff1c93a9a7a313437ee42f9143a1c57b92f0cda
eviatarLevi <levieviatar@gmail.com>
2018-06-11 14:18:08 +0200

    fix

--- TestCase.hpp
+++ TestCase.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <boost/type_index.hpp>
 using namespace std;
 
 class TestCase {
@@ -8,54 +9,68 @@ class TestCase {
    ostream &p;
    int testT;
    int testF;
+   int count;
    public:
-   TestCase(const string s,ostream &p) : s(s),p(p){testT = 0; testF = 0;}
+   TestCase(const string s,ostream &p) : s(s),p(p){testT = 0; testF = 0; count = 0;}
    template <typename T>
    TestCase check_equal(T a,T b)
    {
-       if(!(a==b)) p<<"erorr equal"<<endl;
+       count++;
+       if(!(a==b)){
+           testF++;
+            p<< s << ": Failure in test #" << count << ": "
+            << a << " should equal "<< b << "!" << endl;
+       } 
+       else testT++;
        return *this;
    }
    template <typename T>
    TestCase check_different(T a,T b)
    {
-       if(!(a!=b)) p<<"erorr different"<<endl;
+       count++;
+       if(!(a!=b)) {
+           testF++;
+            p<< s << ": Failure in test #" << count << ": "
+            << a << " should different "<< b << "!" << endl;
+       } 
+       else testT++;
        return *this;
    }
    template <typename T>
    TestCase check_output(T a,string b)
    {
+       count++;
        ostringstream t;
        t << a;
        string tests = t.str();
-       if(tests != b) p << "erorr output" <<endl; 
+       if(tests != b) 
+       {
+           testF++;
+            p<< s << ": Failure in test #" << count << ": string value should be "
+            << b << " but is "<< a << endl;
+       } 
+       else testT++;
        return *this;
    }
-   template <typename T>
-   TestCase check_function(int (f)(T),T a, T b)
-   {
-       return *this;
-   } 
-    template <typename T1,typename T2>
-   TestCase check_function(int (f)(T2),T2 a, T1 b)
-   {
-       return *this;
-   } 
      template <typename T1,typename T2,typename T3>
-   TestCase check_function(int (f)(const T2),T3 a, T1 b)
+   TestCase check_function(T1 (f),T2 a, T3 b)
    {
+       count++;
+       if(!(f(a)==b)){
+           testF++;
+           p<< s << ": Failure in test #" << count << ": Function should return " <<
+            b << " but returned "<< f(a) << "!" << endl;
+       }
+       else testT++;
        return *this;
    } 
-      template <typename T1,typename T2,typename T3>
-   TestCase check_function( T2 f,T3 a, T1 b)
-   {
-       return *this;
-   } 
+
 
 
    void print()
    {
-
+    p << s << ": " << testF << " failed, " << 
+        testT << " passed, " << count << " total." << endl << "---" << endl;
    }
 
 
